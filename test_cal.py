510beec17c90c30e1ae9533518b35f5b4b1a01a6
shalinis97 <shalinis1487@gmail.com>
2026-02-25 22:52:10 +0530

    test files

--- test_cal.py
+++ test_cal.py
@@ -1,12 +1,12 @@
 from calculator import add, subtract
 
 def test_add(a,b):
-    assert add(2,3) == 5
+    assert add(2, 3) == 5
 
 def test_subtract(a,b):
-    assert subtract(3,2) == 1
+    assert subtract(5, 3) == 2
 
 if __name__ == "__main__":
     test_add()
     test_subtract()
-    print("All test passed!")
+    print("All tests passed!")
